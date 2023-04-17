0e0bcb85e240e36e61d54d6142ef4c317325d48d
smdletsh021 <smdletsh021@student.wethinkcode.co.za>
2023-04-17 03:49:34 +0200

    initial commit

---
+++ src/main/java/com/ssm/hr360/Hr360Application.java
@@ -0,0 +1,13 @@
+package com.ssm.hr360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hr360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hr360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssm/hr360/Hr360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssm.hr360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hr360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
